77dbef5d4a828867de609dffe2387368fea8d412
Raine Revere <raineorshine@gmail.com>
2018-11-06 08:41:45 -0700

    Search local and/or online.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const SEARCH_TOKEN = '__SEARCH__'
 let windowCounter = 0
 
-const bookSellers = [
+const localSellers = [
   {
     name: 'City Stacks',
     search: 'https://www.citystacks.com/search/site/__SEARCH__'
@@ -16,7 +16,10 @@ const bookSellers = [
   {
     name: 'Boulder Bookstore',
     search: 'https://www.boulderbookstore.net/search/site/__SEARCH__'
-  },
+  }
+]
+
+const onlineSellers = [
   {
     name: 'Better World Books',
     search: 'https://www.betterworldbooks.com/search/results?q=__SEARCH__'
@@ -35,11 +38,16 @@ const searchUrl = (seller, search) =>
   seller.search.replace(SEARCH_TOKEN, window.encodeURIComponent(search))
 
 const sellerLink = (seller, search) =>
-  <a key={seller.name} target='_blank' rel='noopener noreferrer' href={searchUrl(seller, search)}>{seller.name}</a>
+  <a disabled={!search} key={seller.name} target='_blank' rel='noopener noreferrer' href={searchUrl(seller, search)}>{seller.name}</a>
 
-const searchAll = search => {
+const searchAll = (search, local, online) => {
   // local only
-  bookSellers.slice(0, 3).forEach(seller =>
+  const sellers = [].concat(
+    local ? localSellers : [],
+    online ? onlineSellers : []
+  )
+
+  sellers.forEach(seller =>
     window.open(searchUrl(seller, search), windowCounter++)
   )
 }
@@ -49,7 +57,9 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      search: ''
+      search: '',
+      local: true,
+      online: false
     }
   }
 
@@ -61,10 +71,29 @@ class App extends Component {
     return (
       <div className='App'>
         <div className='App-header'>
-          <input type='text' autoFocus placeholder='Search bookstores' value={this.state.search} onChange={e => this.setState({ search: e.target.value })} onKeyDown={e => e.key === 'Enter' ? searchAll(this.state.search) : null} />
-          <p className='helper-text'>Hit Enter to open windows for local bookstores.</p>
-          <div className='list'>
-            {bookSellers.map(sellerLinkWithSearch)}
+          <div>
+
+            <input className='search' type='text' autoFocus placeholder='Search bookstores' value={this.state.search} onChange={e => this.setState({ search: e.target.value })} onKeyDown={e => e.key === 'Enter' ? searchAll(this.state.search, this.state.local, this.state.online) : null} />
+
+            <p className='helper-text'>Hit Enter to search:</p>
+
+            <div className='scope'>
+              <div>
+                <input type='checkbox' checked={this.state.local} onChange={() => this.setState({ local: !this.state.local })} /> Local
+              </div>
+              <div>
+                <input type='checkbox' checked={this.state.online} onChange={() => this.setState({ online: !this.state.online })} /> Online
+              </div>
+            </div>
+
+            <div className='sellers sellers-local'>
+              {localSellers.map(sellerLinkWithSearch)}
+            </div>
+
+            <div className='sellers sellers-online'>
+              {onlineSellers.map(sellerLinkWithSearch)}
+            </div>
+
           </div>
         </div>
       </div>
